7509d3a34167f611a21f2e913e3b5d113a1ef351
marceloh-dev <marcelohs.dev@gmail.com>
2022-07-04 17:46:55 -0300

    Starting Spring Boot aplication

---
+++ src/main/java/keepgoing/keepgoing/KeepgoingApplication.java
@@ -0,0 +1,13 @@
+package keepgoing.keepgoing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeepgoingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeepgoingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/keepgoing/keepgoing/KeepgoingApplicationTests.java
@@ -0,0 +1,13 @@
+package keepgoing.keepgoing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeepgoingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
